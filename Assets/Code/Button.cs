62fb1c13d89603c018ce00e933854ca820d764b9
Daniel <daniel.maansson@gmail.com>
2017-02-25 19:49:50 +0100

    cool stuff

---
+++ Assets/Code/Button.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+	public List<Interactable> m_targets;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		foreach (var target in m_targets)
+		{
+			target.OnEnter();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		foreach (var target in m_targets)
+		{
+			target.OnExit();
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		//print(other.tag);
+		if (other.tag == "Player")
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				foreach (var target in m_targets)
+				{
+					target.OnPressed();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Code/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+	public GameObject m_cableRoot;
+	public Material m_onMat;
+	public Material m_offMat;
+
+	public abstract void OnPressed();
+	public abstract void OnEnter();
+	public abstract void OnExit();
+
+	public void SetCableState(bool state)
+	{
+		var renderers =  m_cableRoot.GetComponentsInChildren<MeshRenderer>();
+		foreach (var renderer in renderers)
+		{
+			renderer.material = state ? m_onMat : m_offMat;
+		}
+	}
+}
